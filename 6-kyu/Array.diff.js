e59569e6d03f9164084e75cf42ca75698fee96c9
Onur Karaaş <onurkaraas52@gmail.com>
2021-08-30 14:55:18 +0300

    Update Array.diff.js

--- 6-kyu/Array.diff.js
+++ 6-kyu/Array.diff.js
@@ -1,10 +1,1 @@
-function arrayDiff(a, b) {
-  for (let i = 0; i < a.length && i < b.length; i++) {
-    if (a.includes(b[i])) {
-      const isLargeNumber = (element) => element === b[i];
-      let sp = a.findIndex(isLargeNumber);
-      a.splice(sp, 1);
-      console.log(a);
-    }
-  }
-}
+
